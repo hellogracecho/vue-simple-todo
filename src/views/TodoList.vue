6fb33c33d9c6b3df07ef8152c768c1c557b02c0a
Grace Cho <grace@splitmango.com>
2021-10-22 15:47:08 -0700

    previous changes

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -8,12 +8,13 @@
         type="text"
         placeholder="Add new todo"
       />
-      <button @click="addTodo(newTodo)">Add</button>
+      <button @click="addTodo(newTodo)" class="btn--primary">Add</button>
     </div>
     <ul>
       <TodoItem
         @clicked="onClickChild"
-        v-for="item in todoList"
+        @removed="onRemoveTodo"
+        v-for="item in reversedList"
         :todo="item"
         :key="item.id"
       ></TodoItem>
@@ -25,6 +26,7 @@
 <script>
 // @ is an alias to /src
 import TodoItem from '@/components/TodoItem.vue';
+import { todoList } from '@/data';
 
 export default {
   name: 'TodoList',
@@ -34,14 +36,7 @@ export default {
   props: ['checkedTodo'],
   data() {
     return {
-      todoList: [
-        { id: 0, text: 'Wake up in the morning', isChecked: false },
-        { id: 1, text: 'Brush my teeth', isChecked: false },
-        { id: 2, text: 'Wash your face', isChecked: false },
-        { id: 3, text: 'Eat breakfast', isChecked: false },
-        { id: 4, text: 'Do laundry', isChecked: false },
-        { id: 5, text: 'Vacuum', isChecked: false },
-      ],
+      todoList: todoList,
       newTodo: '',
       checkedTodos: [],
     };
@@ -67,11 +62,25 @@ export default {
       });
       this.reOrderList();
     },
+    onRemoveTodo(selectedItem) {
+      if (confirm('Are you sure you wanna delete?')) {
+        this.todoList.forEach((item, index) => {
+          if (item.id === selectedItem.id) {
+            this.todoList.splice(index, 1);
+          }
+        });
+      }
+    },
     reOrderList: function() {
       // order by unchecked todos first followed by completed ones
       this.todoList.sort(function(x, y) {
-        return x.isChecked === y.isChecked ? 0 : x.isChecked ? 1 : -1;
+        return x.isChecked === y.isChecked ? 0 : x.isChecked ? -1 : 1;
       });
+    },
+  },
+  computed: {
+    reversedList() {
+      return [...this.todoList].reverse();
     },
   },
 };
@@ -85,7 +94,7 @@ export default {
     padding: 0;
   }
   li {
-    margin: 10px 0;
+    margin: 16px 0;
   }
 
   &_add {
@@ -100,21 +109,6 @@ export default {
       &:focus {
         outline: none !important;
         box-shadow: 0 0 2px #00c400;
-      }
-    }
-    button {
-      cursor: pointer;
-      padding: 10px 20px;
-      background-color: #00c400;
-      border: transparent;
-      color: #fff;
-      font-weight: bold;
-      border-radius: 4px;
-
-      &:hover,
-      &:focus,
-      &:active {
-        background-color: #0d9b0d;
       }
     }
   }
